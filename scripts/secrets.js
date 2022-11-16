01ed416df85e5d792fb0c55701f4fc13235e6509
Barış DEMİRCİ <demirci.baris@gmail.com>
2022-11-16 10:52:47 +0300

    💚 Check VAUL_ env variables first

--- scripts/secrets.js
+++ scripts/secrets.js
@@ -4,9 +4,19 @@ const fs = require("fs");
 const path = require("path");
 
 const secrets_path = path.resolve(__dirname, "..", ".env");
+
+if (!fs.existsSync(secrets_path)) {
+	fs.writeFileSync(secrets_path, "");
+}
 const secrets_file = fs.readFileSync(secrets_path, "utf8");
 
 const saved_secrets = dotenv.parse(secrets_file);
+
+if (!saved_secrets.VAULT_TOKEN && process.env.VAULT_TOKEN)
+	saved_secrets.VAULT_TOKEN = process.env.VAULT_TOKEN;
+
+if (!saved_secrets.VAULT_URL && process.env.VAULT_URL)
+	saved_secrets.VAULT_URL = process.env.VAULT_URL;
 
 const token = saved_secrets.VAULT_TOKEN;
 const url = saved_secrets.VAULT_URL;
