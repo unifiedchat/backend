bba26560da598e2c85bafbe52e940a7025e6ef54
Barış DEMİRCİ <demirci.baris@gmail.com>
2022-11-16 11:10:38 +0300

    🔐 Validate env variables

--- src/config.ts
+++ src/config.ts
@@ -1,28 +1,44 @@
 import { config } from "dotenv";
+import { cleanEnv, num, str } from "envalid";
 
 config();
 
+const env = cleanEnv(process.env, {
+	NODE_ENV: str({ default: "development" }),
+	PORT: num({ default: 3000 }),
+	API_VERSION: str({ default: "v1" }),
+	POSTGRES_HOST: str(),
+	POSTGRES_PORT: num(),
+	POSTGRES_USER: str(),
+	POSTGRES_PASSWORD: str(),
+	POSTGRES_DB_NAME: str(),
+	GOOGLE_CLIENT_ID: str(),
+	GOOGLE_CLIENT_SECRET: str(),
+	GOOGLE_REDIRECT_URI: str(),
+	SECRET: str(),
+});
+
 export const factory = () => {
 	return {
-		PORT: parseInt(process.env.PORT),
-		API_VERSION: process.env.API_VERSION,
+		PORT: env.PORT,
+		API_VERSION: env.API_VERSION,
 		POSTGRES: {
-			host: process.env.POSTGRES_HOST,
-			port: parseInt(process.env.POSTGRES_PORT),
-			username: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-			database: process.env.POSTGRES_DB_NAME,
+			host: env.POSTGRES_HOST,
+			port: env.POSTGRES_PORT,
+			username: env.POSTGRES_USER,
+			password: env.POSTGRES_PASSWORD,
+			database: env.POSTGRES_DB_NAME,
 		},
 		GOOGLE: {
-			clientID: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			redirectURI: process.env.GOOGLE_REDIRECT_URI,
+			clientID: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+			redirectURI: env.GOOGLE_REDIRECT_URI,
 		},
 		API_URLS: {
 			youtube: "https://www.googleapis.com/youtube/v3/",
 			twitch: "https://api.twitch.tv/helix",
 		},
-		SECRET: process.env.SECRET,
+		SECRET: env.SECRET,
 		EPOCH: new Date("2022-11-15T00:00:00Z").getTime(),
 	};
 };
